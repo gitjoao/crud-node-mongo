ccb2bca8ac8f65d0e183e5e7cefee8c174a798b2
jgbatista <joao.moraes.batista@hotmail.com>
2021-12-28 01:44:47 -0300

    ajuste

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import { CreateUsersController } from './controllers/CreateUsersController'
 
 const routes = Router()
 
+routes.get('/', (request: Request, response: Response) => {
+  response.json({data: 'Server ready'})
+})
 routes.post('/users', new CreateUsersController().handle)
 
 export { routes }
