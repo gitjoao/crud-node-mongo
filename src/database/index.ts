59fef1ba73adc60bcf4f9bf7d57f10305340de2e
jgbatista <joao.moraes.batista@hotmail.com>
2021-12-28 11:36:43 -0300

    teste

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,3 @@
-import { ConnectionOptions, createConnection } from 'typeorm'
+import { createConnection } from 'typeorm'
 
-const options: ConnectionOptions = {
-  type: 'sqlite',
-  database: './mydb.sql',
-  entities: ["build/**/*.entity{.ts,.js}"],
-  migrations: ["build/**/*.migration{.ts,.js}"],
-}
-
-createConnection(options)
+createConnection()
